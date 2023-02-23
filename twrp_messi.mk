81acd11ac5f9c0b48066ea47fc6f287f5764b7d8
Carlo Dee <carlodee.official@proton.me>
2023-02-23 21:34:05 +0800

    Update: Cleanup. * Renamed device makefile. * Removed my_xxxxxxxxx partitions in recovery.fstab.
    Co-authored-by: Carlo Dandan <jasminecarlo01@gmail.com>
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- twrp_messi.mk
+++ twrp_messi.mk
@@ -10,15 +10,8 @@ PRODUCT_MODEL := Realme 9 4G RMX3521
 PRODUCT_PLATFORM := bengal
 PRODUCT_MANUFACTURER := realme
 
-PRODUCT_GMS_CLIENTID_BASE := android-$(PRODUCT_MANUFACTURER)
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="messi-user 12 SKQ1.220303.001 1671110188642 release-keys"
-
-BUILD_FINGERPRINT := $(PRODUCT_MANUFACTURER)/$(PRODUCT_RELEASE_NAME)/RMX3521:12/SKQ1.220303.001/1671110188642:user/release-keys
-
 # Inherit from RMX3521 Device
-$(call inherit-product, device/realme/messi/device_messi.mk)
+$(call inherit-product, device/realme/messi/messi.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
